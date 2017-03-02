772269ad0fdcd94e60dfcb7f8e1ca6773752f3f6
Hoang Le <lhoang295@gmail.com>
2017-03-02 19:20:06 +0700

    first commit

---
+++ build/scripts/clone.sh
@@ -0,0 +1,2 @@
+git clone -b staging git@github.com:starlightgroup/node-api.git app-server/node-api
+git clone -b staging git@github.com:starlightgroup/flashlightsforever.git app-server/flashlightsforever
---
+++ build/scripts/launch.sh
@@ -0,0 +1,5 @@
+docker kill $(docker ps -q) || true
+docker rm $(docker ps -a -q) || true
+docker rmi $(docker images -q -f dangling=true) || true
+docker-compose build 
+docker-compose up -d
